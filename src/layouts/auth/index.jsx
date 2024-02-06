e4481da9a5551f8b6c3c7bedf9095696484963c0
agnesjuliana <agafordev@gmail.com>
2024-02-06 11:19:39 +0700

    ✨feat: login and register page

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -3,24 +3,36 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import routes from "../../routes";
 import Login from "../../views/auth/login";
 
+// import image
+import loginAsset from "../../assets/auth/login-asset.png";
+
 function AuthLayout() {
   return (
-    <Routes>
-      {routes.map((prop, key) => {
-        if (prop.layout === "/auth") {
-          return (
-            <Route
-              path={prop.path}
-              element={prop.component}
-              key={key}
-            />
-          );
-        } else {
-          return null;
-        }
-      })}
-      <Route path="*" element={<Navigate to="/auth/login" />} />
-    </Routes>
+    <>
+      <div className="grid grid-cols-2">
+      <div>
+          <Routes>
+            {routes.map((prop, key) => {
+              if (prop.layout === "/auth") {
+                return (
+                  <Route path={prop.path} element={prop.component} key={key} />
+                );
+              } else {
+                return null;
+              }
+            })}
+            <Route path="*" element={<Navigate to="/auth/login" />} />
+          </Routes>
+        </div>
+
+        <div>
+          {/* <h1 className="text-3xl font-bold underline">
+            Hello world! AuthLayout
+          </h1> */}
+          <img src={loginAsset} alt="login asset" />
+        </div>
+      </div>
+    </>
   );
 }
 
